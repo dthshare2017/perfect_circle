b875bac3059f68dbaef7ebda903b132e88f9dbb5
dthshare2017 <dthshare2017@gmail.com>
2018-05-24 13:37:18 +0700

    full-code

---
+++ perfect_number/perfect_number/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfect_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please put your number to check isPerfectNumber here: ");
+            int n = int.Parse(Console.ReadLine());
+            if (is_perfect_number(n))
+                Console.WriteLine(n + " is perfect number");
+            else Console.WriteLine(n + "is not perfect number");
+            Console.ReadLine();
+        }
+
+        static bool is_perfect_number(int n)
+        {
+            try
+            {
+                int _total = 0;
+                for (int i = 1; i < n; i++)
+                {
+                    if (n % i == 0)
+                        _total += i;
+                }
+                if (_total == n)
+                    return true;
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
